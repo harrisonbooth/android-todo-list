006e7c7b9dee510d375baa793f987e04dcbfe3c0
Harrison Booth <harrisonbooth@ymail.com>
2017-01-24 12:05:50 +0000

    comment ActivityNewTask

--- app/src/main/java/example/codeclan/com/trevies_todo_list_app/ActivityNewTask.java
+++ app/src/main/java/example/codeclan/com/trevies_todo_list_app/ActivityNewTask.java
@@ -18,6 +18,8 @@ public class ActivityNewTask extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        // Gets saved theme settings and applies them, if there are none, default is applied
         String themeName = SavedThemePreferences.getStoredTheme(this);
         if(themeName == null || themeName.equals("Default")){
             setTheme(R.style.AppTheme);
@@ -27,29 +29,39 @@ public class ActivityNewTask extends AppCompatActivity {
             setTheme(R.style.AppTheme_Lab);
         }
 
+        // Sets layout for activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
 
+        // Sets view variables to the views in the layout
         headlineEditText = (EditText) findViewById(R.id.headline_edit_text);
         descriptionEditText = (EditText) findViewById(R.id.description_edit_text);
+
     }
 
     public void onSaveNewTaskButtonPressed(View button) {
+
+        // Creates intent to send user to the active tasklist activity
         Intent parentIntent = new Intent();
         parentIntent.setClass(this, ActivityTasklist.class);
 
+        // Sets newTaskHeadline and newTaskDescription to what the user has types in the EditTexts
         String newTaskHeadline = headlineEditText.getText().toString();
         String newTaskDescription = descriptionEditText.getText().toString();
 
+        // Creates new task with the information in the newTask string variables
         Task newTask = new Task(newTaskHeadline, newTaskDescription);
 
+        // Sets taskList variable to saved taskList
         TaskList taskList;
         taskList = SavedTaskListPreferences.getStoredTaskList(this);
 
-
+        // Adds new task to taskList, then saves it again
         taskList.addTask(newTask);
         SavedTaskListPreferences.setStoredTaskList(this, taskList);
 
+        // Sends user to active tasklist activity with intent
         startActivity(parentIntent);
+
     }
 }
